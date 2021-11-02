3a966dad7182caaaa0eb5d2b372ef38caee34482
nonz250 <hosaka.non.work@gmail.com>
2021-11-03 05:46:17 +0900

    add my face component.

---
+++ frontend/components/atoms/MyFace.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class="v-MyFace" src="images/face.jpg" alt="">
+</template>
+
+<script>
+export default {
+  name: 'MyFace'
+}
+</script>
+
+<style scoped lang="scss">
+.v-MyFace {
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+}
+</style>
