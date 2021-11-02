f64575959f01062acc08fb62daa5d670f670987e
nonz250 <hosaka.non.work@gmail.com>
2021-11-03 05:46:05 +0900

    add anchor component.

---
+++ frontend/components/atoms/Anchor.vue
@@ -0,0 +1,25 @@
+<template>
+  <a :href="link" target="_blank" rel="noopener noreferrer" class="v-Anchor">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'Anchor',
+  props: {
+    link: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "assets/scss/variables";
+
+.v-Anchor {
+  color: $lab-blue;
+}
+</style>
