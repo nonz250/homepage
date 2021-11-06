536354915680bb3393169d639f0fb3cb4f088456
nonz250 <hosaka.non.work@gmail.com>
2021-11-07 02:51:02 +0900

    add work component.

---
+++ frontend/components/parts/Work.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="v-Work">
+    <anchor :link="link">
+      <p>{{ name }}</p>
+      <img :src="imagePath" alt="" class="image">
+    </anchor>
+  </div>
+</template>
+
+<script>
+import Anchor from '~/components/atoms/Anchor'
+export default {
+  name: 'Work',
+  components: { Anchor },
+  props: {
+    imagePath: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.v-Work {
+  width: 350px;
+  display: grid;
+  align-items: center;
+}
+
+.image {
+  width: 100%;
+}
+</style>
