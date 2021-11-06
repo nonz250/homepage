992d26ea0b5b3c7b80ad7ad79b7844a9194d2918
nonz250 <hosaka.non.work@gmail.com>
2021-11-07 02:50:36 +0900

    fix header.

--- frontend/components/TheHeader.vue
+++ frontend/components/TheHeader.vue
@@ -1,13 +1,148 @@
 <template>
-  <header />
+  <header class="v-Header">
+    <div class="actions" :class="{ hide: !hideScrollingActions }">
+      <div class="left" />
+      <div class="center">
+        <a href="#about" class="actions-link mr-1">
+          About
+        </a>
+        <a href="#service" class="actions-link mr-1">
+          Service
+        </a>
+        <a href="#works" class="actions-link mr-1">
+          Works
+        </a>
+        <anchor link="https://labo.nozomi.bike" :shine="false" class="actions-link">
+          Blog
+        </anchor>
+      </div>
+      <div class="right" />
+    </div>
+
+    <div class="scrolling-actions" :class="{ hide: hideScrollingActions }">
+      <div class="left" />
+      <div class="center" />
+      <div class="right">
+        <div class="triangle-wrap">
+          <div class="triangle" />
+          <div class="content">
+            <anchor link="https://github.com/nonz250" :shine="false">
+              <font-awesome-icon :icon="['fab', 'github']" class="sns-icon github" />
+            </anchor>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
+import Anchor from '~/components/atoms/Anchor'
+
 export default {
-  name: 'TheHeader'
+  name: 'TheHeader',
+  components: { Anchor },
+  data () {
+    return {
+      FPS: 60,
+      SCROLL_TOP_POSITION: 30,
+      currentScrollY: 0,
+      hideScrollingActions: true
+    }
+  },
+  mounted () {
+    this.setCurrentScrollPositionY()
+    document.addEventListener('scroll', this.setCurrentScrollPositionY)
+    setInterval(() => {
+      this.setHideScrollingActions()
+    }, 1000 / this.FPS)
+  },
+  methods: {
+    setCurrentScrollPositionY () {
+      this.currentScrollY = window.scrollY
+    },
+    setHideScrollingActions () {
+      this.hideScrollingActions = this.currentScrollY <= this.SCROLL_TOP_POSITION
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "assets/scss/variables";
 
+.v-Header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.actions-link {
+  color: $lnk-black;
+  font-weight: bold;
+  font-size: 1.5rem;
+  @media screen and (max-width: 600px) {
+    font-size: 1rem;
+  }
+}
+
+.scrolling-actions {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.hide {
+  display: none;
+}
+
+.center {
+  display: flex;
+  justify-content: flex-end;
+  align-content: center;
+}
+
+.right {
+  display: flex;
+  justify-content: flex-end;
+  align-content: center;
+}
+
+.sns {
+  &-icon {
+    font-size: 2rem;
+    margin-right: .5rem;
+
+    &.github {
+      color: $black !important;
+    }
+  }
+}
+
+.triangle-wrap {
+  position: relative;
+}
+
+.triangle {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border-top: 5rem solid #e5e5e5;
+  border-left: 5rem solid transparent;
+  z-index: 1000;
+}
+
+.content {
+  position: absolute;
+  top: .5rem;
+  right: 0;
+  z-index: 1100;
+}
 </style>
