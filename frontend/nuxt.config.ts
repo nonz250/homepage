d49ccb3262f67467460847e350e343be793f86bd
nonz250 <hosaka.non.work@gmail.com>
2023-01-07 07:51:24 +0900

    add ogp setting.

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -15,8 +15,18 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
-        { name: 'format-detection', content: 'telephone=no' }
+        { name: 'description', content: 'https://nozomi.bike' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content: '@nonz250' },
+        { name: 'fb:app_id', content: '370746857063202' },
+        { name: 'og:type', content: 'website' },
+        { name: 'og:site_name', content: 'Nozomi Hosaka' },
+        { name: 'og:title', content: 'Nozomi Hosaka' },
+        { name: 'og:description', content: 'https://nozomi.bike' },
+        { name: 'og:url', content: 'https://nozomi.bike' },
+        { name: 'og:image', content: 'https://nozomi.bike/images/homepage-ogp.webp' },
+        { name: 'og:image:alt', content: 'Business card' },
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
