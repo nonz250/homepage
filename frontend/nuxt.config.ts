811c205578169f681e0f14f65e22d05b3466bef6
nonz250 <hosaka.non.work@gmail.com>
2023-01-07 06:13:01 +0900

    available nuxt3.

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   css: [
     '@/assets/scss/app.scss',
     '@fortawesome/fontawesome-svg-core/styles.css',
-    'material-icons/iconfont/material-icons.css'
+    'material-icons/iconfont/material-icons.css',
+    '@fontsource/noto-sans-jp/japanese.css',
   ],
 })
--- frontend/plugins/fontawesome.ts
+++ frontend/plugins/fontawesome.ts
@@ -1,7 +1,6 @@
 // For Nuxt 3
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt worry about the CSS
@@ -9,7 +8,6 @@ config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fas)
 library.add(fab)
 
 export default defineNuxtPlugin((nuxtApp) => {
